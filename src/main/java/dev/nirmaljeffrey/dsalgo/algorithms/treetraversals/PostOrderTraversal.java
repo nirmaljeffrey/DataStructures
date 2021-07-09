f8b0bb52bb57de94154a336bcb711efa79658c24
Nirmal Jeffrey <jeffattry@gmail.com>
2021-07-09 23:03:02 +0530

    feat: PostOrderTraversal Iterative implementation

--- src/main/java/dev/nirmaljeffrey/dsalgo/algorithms/treetraversals/PostOrderTraversal.java
+++ src/main/java/dev/nirmaljeffrey/dsalgo/algorithms/treetraversals/PostOrderTraversal.java
@@ -3,6 +3,7 @@ package dev.nirmaljeffrey.dsalgo.algorithms.treetraversals;
 import dev.nirmaljeffrey.dsalgo.common.BinaryTreeNode;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class PostOrderTraversal {
 
@@ -16,6 +17,25 @@ public class PostOrderTraversal {
     }
 
     public static <T> void iterativeTraversal(ArrayList<T> arrayList, BinaryTreeNode<T> rootNode) {
-      //TODO
+      BinaryTreeNode<T> currentNode = rootNode;
+      Stack<BinaryTreeNode<T>> stack = new Stack<>();
+      while (currentNode != null || !stack.isEmpty()) {
+          if (currentNode != null) {
+              stack.push(currentNode);
+              currentNode = currentNode.left;
+          } else {
+              BinaryTreeNode<T> temp = stack.peek().right;
+              if (temp == null) {
+                temp = stack.pop();
+                arrayList.add(temp.data);
+                while (!stack.isEmpty() && temp == stack.peek().right) {
+                    temp = stack.pop();
+                    arrayList.add(temp.data);
+                }
+              } else {
+                  currentNode = temp;
+              }
+          }
+        }
     }
 }
