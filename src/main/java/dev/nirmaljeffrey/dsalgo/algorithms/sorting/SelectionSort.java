fe95415fa56212c384dcef637c3659fd7a50da42
Nirmal Jeffrey <jeffattry@gmail.com>
2021-07-10 23:27:59 +0530

    feat: SelectionSort implementation

---
+++ src/main/java/dev/nirmaljeffrey/dsalgo/algorithms/sorting/SelectionSort.java
@@ -0,0 +1,22 @@
+package dev.nirmaljeffrey.dsalgo.algorithms.sorting;
+
+public class SelectionSort {
+
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    minIndex = j;
+                }
+            }
+          swap(arr, i, minIndex);
+        }
+    }
+
+    private static void swap(int[] arr, int firstIndex, int secondIndex) {
+        int temp = arr[firstIndex];
+        arr[firstIndex] = arr[secondIndex];
+        arr[secondIndex] = temp;
+    }
+}
