14cdfad405c3615c3c8e91c8a0d1290645a113c5
Nirmal Jeffrey <jeffattry@gmail.com>
2021-07-12 08:01:47 +0530

    feat: InsertionSort implementation

---
+++ src/main/java/dev/nirmaljeffrey/dsalgo/algorithms/sorting/InsertionSort.java
@@ -0,0 +1,15 @@
+package dev.nirmaljeffrey.dsalgo.algorithms.sorting;
+
+public class InsertionSort {
+    public static  void sort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int value = array[i];
+            int holeIndex = i;
+            while (holeIndex > 0 && array[holeIndex - 1] > value) {
+                array[holeIndex] = array[holeIndex - 1];
+                --holeIndex;
+            }
+            array[holeIndex] = value;
+        }
+    }
+}
